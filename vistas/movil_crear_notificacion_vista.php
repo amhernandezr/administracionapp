1bc66967f29bc028d955f08384d3bed9e2339593
amhernandezr <amhernandezr@unah.hn>
2021-06-25 14:12:50 -0600

    Create movil_crear_notificacion_vista.php

---
+++ vistas/movil_crear_notificacion_vista.php
@@ -0,0 +1,72 @@
+<?php 
+
+?>
+
+{
+    background-color: rgba();
+    <font-family: Arial;
+    margin:100;
+    padding:100;
+    padding-top:100px;
+}
+
+section {
+    background-color:white;
+    border: 5px solid black;
+    height: auto;
+    padding:20px;
+    box-sizing: border-box;
+    color : black;
+    margin: 100 auto;
+    font-size: 50px
+}
+    <section>
+    <body>
+    </style>
+<?php
+
+?>
+<div class="container-fluid">
+<div class="row">
+<div class=col-md-12 text-rigth>
+<h1>Crear Notificacion</h1>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-6 caja col-centrar">
+
+
+<form action="" enctype="multipart/form-data" 
+method = "POST"  role="form">
+<legend>Registrarse</legend>
+
+<div class="form-group">
+<input name="Titulo:" type="text" 
+class ="form-control" id="" placeholder="Ingrese Titulo de la notificacion...">
+</div>
+<br>
+<div class="form-group">
+<input name="Contenido:" type="text" 
+class="form-control" id=""placeholder ="Ingrese Contenido de la notificacion...">
+ </div>
+<br>
+
+<optgroup>
+<optgroup Label="Elija un Segmento">
+<option value="">Noticia</option>
+<option value="">Notificacion</option>
+<option value="">Chat</option>
+</optgroup>
+
+<input type="datetime-local">
+
+
+ <button type="submit" class="btn-btn-primary">Vista Previa</button>
+ <button type="submit" class="btn-btn-primary">Publicar</button>
+</form>
+</div>
+</div>
+
+</section>
+
+<?php 
